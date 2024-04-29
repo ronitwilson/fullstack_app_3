48358ba965611f9e3f11614ca3f9aa661d7b3b2e
Ronit Wilson <ronit.study@gmail.com>
2024-04-29 23:25:07 +0530

    add login form

--- airbnb-clone/src/pages/LoginPage.jsx
+++ airbnb-clone/src/pages/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
     return(
         <div className="mt-4 grow flex items-center justify-around">
@@ -6,7 +8,12 @@ export default function LoginPage() {
             <form  className="max-w-md mx-auto"> 
                 <input type='text' placeholder='your@email.com' />
                 <input type='password' placeholder='password' />
+                <input type='text' placeholder='admin' />
                 <button className="primary">Login</button>
+                <div className="text-center py-2">
+                    Don't hanve an account? 
+                    <Link to={'/register'}> Register now</Link>
+                </div>
           </form>
         </div>
         </div>
