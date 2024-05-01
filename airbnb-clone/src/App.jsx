05c8a8ee90d646c0d48b98e31632d1bb9918647f
Ronit Wilson <ronit.study@gmail.com>
2024-05-01 12:17:27 +0530

    implement register page

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import IndexPage from './pages/IndexPage'
+import RegisterPage from './pages/RegisterPage'
 import Layout from './Layout'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path='/'  element={<Layout />} > 
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
   )
---
+++ airbnb-clone/src/pages/RegisterPage.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+    return(
+        <div className="mt-4 grow flex items-center justify-around">
+        <div className="mb-64">
+            <h1 className="text-4xl text-center mb-4">Register</h1>
+            <form  className="max-w-md mx-auto"> 
+                <input type='text' placeholder='your@email.com' />
+                <input type='password' placeholder='password' />
+                <button className="primary">Login</button>
+                <div className="text-center py-2">
+                    Have an account? 
+                    <Link to={'/login'}> Login now</Link>
+                </div>
+          </form>
+        </div>
+        </div>
+    )
+}
