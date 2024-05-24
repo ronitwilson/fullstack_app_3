4654526f25a22a2c7e7a0c5ed8f650d20bf8c0b2
Ronit Wilson <ronit.study@gmail.com>
2024-05-24 07:20:23 +0530

    add user name to header

--- airbnb-clone/src/Header.jsx
+++ airbnb-clone/src/Header.jsx
@@ -1,6 +1,9 @@
 import {Link} from 'react-router-dom';
+import {useContext} from "react";
+import {UserContext} from "./UserContext.jsx";
 
 export default function Header() {
+  const {username} = useContext(UserContext)
     return (
         <div>
             <header className='p-4 flex justify-between'>
@@ -32,6 +35,13 @@ export default function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </div>
+            {
+              !!username &&  (
+                <div>
+                  {username}
+                </div>
+              )
+            }
           </Link>
           </header>
         </div>
