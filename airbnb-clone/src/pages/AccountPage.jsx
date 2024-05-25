1897d24cdd5ce69d2ddb4569e34a7257779d80ce
Ronit Wilson <ronit.study@gmail.com>
2024-05-25 22:48:01 +0530

    display user name at account page

--- airbnb-clone/src/pages/AccountPage.jsx
+++ airbnb-clone/src/pages/AccountPage.jsx
@@ -1,7 +1,13 @@
+import {useState, useContext} from 'react'
+import { UserContext } from '../UserContext'
+
+
 export default function AccountPage() {
+    const{username} = useContext(UserContext)
+
     return (
         <div className=''>
-            account page
+            account page of {username}
         </div>
     )
 }
