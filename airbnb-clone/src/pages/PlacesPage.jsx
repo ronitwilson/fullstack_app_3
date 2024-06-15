c0ae7e492ec24c3ef241443962f4d50ffae659b6
Ronit Wilson <ronit.study@gmail.com>
2024-06-15 19:57:38 +0530

    add description to form

--- airbnb-clone/src/pages/PlacesPage.jsx
+++ airbnb-clone/src/pages/PlacesPage.jsx
@@ -30,9 +30,11 @@ export default function PlacesPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                     </svg> 
                     &nbsp;upload
-
                 </button>
                 </div>
+                <h2> Description </h2>
+                <p className="text-gray-500 text-sm">discription of the place</p>
+                <textarea></textarea>
             </form>
         )}
         </div>
