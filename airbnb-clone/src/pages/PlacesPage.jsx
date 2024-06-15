2a2b3bac15fda829b5a94dd985b5385cf4cab783
Ronit Wilson <ronit.study@gmail.com>
2024-06-15 19:53:57 +0530

    improve the form

--- airbnb-clone/src/pages/PlacesPage.jsx
+++ airbnb-clone/src/pages/PlacesPage.jsx
@@ -12,7 +12,27 @@ export default function PlacesPage() {
         )}
         {action === 'new' && (
             <form>
-                
+                <h2 className="text-2xl mt-4"> Title </h2>
+                <p className="text-gray-500 text-sm">Title for your plance</p>
+                <input type='text' placeholder="title"></input>
+                <h2> Address </h2>
+                <p className="text-gray-500 text-sm">Address for your place</p>
+                <input type='text' placeholder="address"></input>
+                <h2> Photos </h2>
+                <p className="text-gray-500 text-sm">more the better</p>
+                <div className="flex gap-2">
+                    <input type='text' placeholder="add uning link"></input>
+                    <button className="rounded-2xl px-4">Add&nbsp;phtoto</button>
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-7 mt-2">
+                <button className="border rounded-2xl p-3 flex justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3"  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                    </svg> 
+                    &nbsp;upload
+
+                </button>
+                </div>
             </form>
         )}
         </div>
